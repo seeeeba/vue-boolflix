a6d4c1e63e7cd5849baf992001c350ab840811a0
seeeeba <lillo.sebastian1@gmail.com>
2022-01-13 18:37:32 +0100

    progress

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="movie">
-        <div>Titolo: {{moviesInfos.title}}</div> 
-        <div>Titolo originale: {{moviesInfos.original_title}}</div> 
+        <div v-if="moviesInfos.title" >Titolo: {{moviesInfos.title}}</div> 
+        <div v-else>{{moviesInfos.name}}</div>
+        <div v-if="moviesInfos.original_title">Titolo originale: {{moviesInfos.original_title}}</div> 
+        <div v-else>Titolo originale: {{moviesInfos.original_name}}</div>
         <div>Lingua: {{moviesInfos.original_language}}</div> 
         <div>Voto: {{moviesInfos.vote_average}}</div> 
     </div>
